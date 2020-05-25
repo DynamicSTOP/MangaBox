1bbb6810a75594c0a59af75c888ee8111ff7baa5
koms <komsldt@gmail.com>
2020-05-25 10:59:36 +0300

    sort new chapters

--- src/renderer/components/MangaList.vue
+++ src/renderer/components/MangaList.vue
@@ -5,18 +5,45 @@ export default {
   name: 'MangaList',
   components: { Manga },
   computed: {
-    ...mapState(['allManga'])
+    ...mapState(['allManga']),
+    sortedManga () {
+      const s = this.allManga.slice(0).sort((a, b) => {
+        const aNew = a.json.newChapters.indexOf('en') !== -1
+        const bNew = b.json.newChapters.indexOf('en') !== -1
+        if (aNew && !bNew) {
+          return -1
+        } else if (!aNew && bNew) {
+          return 1
+        } else {
+          const al = a.json.newChapters.length > 0
+          const bl = b.json.newChapters.length > 0
+          if (al && !bl) {
+            return -1
+          } else if (!al && bl) {
+            return 1
+          }
+        }
+        return a.id - b.id
+      })
+      console.log(s.map(m => {
+        return {
+          id: m.id,
+          nc: JSON.stringify(m.json.newChapters)
+        }
+      }))
+      return s
+    }
   }
 }
 </script>
 
 <template>
   <div class="mangaList flex-row">
-    <div v-if="allManga.length === 0">
+    <div v-if="sortedManga.length === 0">
       Nothing tracked
     </div>
     <manga
-      v-for="manga in allManga"
+      v-for="manga in sortedManga"
       :key="'manga_'+manga.id"
       :manga="manga"
     />
